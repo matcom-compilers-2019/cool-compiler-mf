46c9b58ffe70360a71c4bc47ee12bd606542f5ae
Jose Luis Alvarez <j.alvarez@estudiantes.matcom.uh.cu>
2019-06-28 15:04:19 -0400

    scripts modified

---
+++ src/coolc.bat
@@ -0,0 +1,5 @@
+@echo coolc v8   
+@echo Copyright (c) 2019: Daniel de la Osa, Jose Luis Alvarez    
+
+@coolc\bin\coolc.exe %*
+::@pause
--- src/coolc.sh
+++ src/coolc.sh
@@ -4,9 +4,10 @@ INPUT_FILE=$1
 OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
 # Si su compilador no lo hace ya, aquí puede imprimir la información de contacto
-echo "LINEA_CON_NOMBRE_Y_VERSION_DEL_COMPILADOR"   # Recuerde cambiar estas
-echo "Copyright (c) 2019: Nombre1, Nombre2, Nombre3"    # líneas a los valores correctos
+echo "coolc v8"   # Recuerde cambiar estas
+echo "Copyright (c) 2019: Daniel de la Osa, Jose Luis Alvarez"    # líneas a los valores correctos
 
 # Llamar al compilador
 # echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
 
+coolc/bin/coolc %*
